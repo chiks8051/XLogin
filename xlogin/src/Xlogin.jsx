65fce3d4ec55755af183380024165ef7b228995c
Kumar Harshvardhan <sharshu1999hraj@gmail.com>
2024-06-27 00:30:22 +0530

    XLogin Implemented-2

--- xlogin/src/Xlogin.jsx
+++ xlogin/src/Xlogin.jsx
@@ -4,11 +4,15 @@ const Xlogin = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLoggedin, setIsloggedin] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleLogin = (event) => {
     event.preventDefault();
     if (username === "user" && password === "password") {
       setIsloggedin(true);
+    } else {
+      setIsloggedin(false);
+      setErrorMessage("Invalid username or password");
     }
   };
 
@@ -18,27 +22,30 @@ const Xlogin = () => {
       {isLoggedin ? (
         <p>Welcome, user!</p>
       ) : (
-        <form>
-          <label>Username:</label>
-          <input
-            type="text"
-            placeholder="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />{" "}
-          <br />
-          <label>Password:</label>
-          <input
-            type="text"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />{" "}
-          <br />
-          <button onClick={handleLogin}>Submit</button>
-        </form>
+        <div>
+          {errorMessage && <p>{errorMessage}</p>}
+          <form>
+            <label>Username:</label>
+            <input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />{" "}
+            <br />
+            <label>Password:</label>
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />{" "}
+            <br />
+            <button onClick={handleLogin}>Submit</button>
+          </form>
+        </div>
       )}
     </div>
   );
